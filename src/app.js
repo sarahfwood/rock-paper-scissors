ac062c75c1829cc4145b7bb76f839f030159646e
Sarah Wood <wood.sarah@outlook.com>
2020-11-11 20:46:23 +0000

    Result function updated

--- src/app.js
+++ src/app.js
@@ -50,10 +50,8 @@ const computerChoice = getComputerChoice();
 
 
 function determineWinner(playerChoice, computerChoice) {
-  if (playerChoice === 'rock' && computerChoice === 'paper') {
-    document.getElementById('result').innerHTML = 'Computer Wins!!';
-  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-    document.getElementById('result').innerHTML = 'You win!!';
+  if (playerChoice === computerChoice) {
+    document.getElementById('result').innerHTML = 'It\'s a tie';
   }
 }
 
